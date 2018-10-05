5971aaeab0b2e1107acc5be9b9a8e135a8475eaf
praveensinghkmr <praveensingh.kmr@gmail.com>
2018-10-05 16:25:39 +0530

    Implemented Logic

--- TwoFer.cs
+++ TwoFer.cs
@@ -2,8 +2,14 @@
 
   public static class TwoFer
   {
-      public static string Name(string input = null)
-      {
-          throw new NotImplementedException("You need to implement this function.");
-      }
-  }
+    public static string Name(string input = null)
+    {
+        if (string.IsNullOrEmpty(input))
+            return Output("you");
+        return Output(input);
+    }
+    public static string Output(string input)
+    {
+        return "One for " + input + ", one for me.";
+    }
+}
